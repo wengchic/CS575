757a910fdbd2eb812d18526c95e0c4eb25326a6b
wengchic <71898164+wengchic@users.noreply.github.com>
2022-12-19 15:18:45 -0800

    Add files via upload

---
+++ OpenCL Array Multiply, Multiply-Add, and Multiply-Reduce/second.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -J 
+#SBATCH -A 
+#SBATCH -p class
+#SBATCH --gres=gpu:1
+#SBATCH -o second.out
+#SBATCH -e second.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=wengchic@oregonstate.edu
+
+
+echo "--------------------------------------------"
+echo "Local Size, Array Size,  MegaPerformance"
+
+for LOCALSIZE in 8 16 32 64 128 256 512
+    do  
+        for NUM_ELEMENTS in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
+            do
+                g++ -DLOCAL_SIZE=$LOCALSIZE -DNUM_ELEMENTS=$NUM_ELEMENTS -o second second.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+                ./second
+            done
+    done
---
+++ OpenCL Array Multiply, Multiply-Add, and Multiply-Reduce/third.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -J 
+#SBATCH -A 
+#SBATCH -p class
+#SBATCH --gres=gpu:1
+#SBATCH -o third.out
+#SBATCH -e third.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=wengchic@oregonstate.edu
+
+
+echo "--------------------------------------------"
+echo "Local Size, Array Size,  MegaPerformance"
+
+for LOCALSIZE in 8 16 32 64 128 256 512
+    do  
+        for NUM_ELEMENTS in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
+            do
+                g++ -DLOCAL_SIZE=$LOCALSIZE -DNUM_ELEMENTS=$NUM_ELEMENTS -o third third.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+                ./third
+            done
+    done
